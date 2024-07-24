645327f4007630e3be86b0812c6d9e572354eea0
Sourav Mazumdar <sourav.mozu@gmail.com>
2024-07-24 15:11:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	
+	cost=0
+	for(i=0;arr.length>1;){
+	    arr.sort()
+		res=arr[0]+arr[1]
+		arr.splice(0,2,res)
+		cost+=res//3 6 10 15
+		console.log(arr,cost)
+	}
+	cost+=res
+	return res
 }
 
 module.exports=mincost;
